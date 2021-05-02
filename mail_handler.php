9de1acd420aebbb58bb3c3abe59098d1d22e39ac
mohsin0176 <mohsin2010ju@gmail.com>
2021-05-02 08:37:12 -0700

    mailer php backend  coded

--- mail_handler.php
+++ mail_handler.php
@@ -1,12 +1,14 @@
 <?php
 	if(isset($_POST['submit'])){
 		$name=$_POST['name'];
-		$to=$_POST['email'];
+		$email=$_POST['email'];
 		$phone=$_POST['phone'];
-		$msg=$_POST['msg'];  
+		$msg=$_POST['msg']; 
+
+		$to='msnju2010@gmail.com'; 
 		$subject='From HR';
 		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-		$headers="From: ".$to;
+		$headers="From: ".$email;
 		if(mail($to, $subject, $message, $headers)){
 			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
 		}
