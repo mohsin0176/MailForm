fc1cbce7c82c69828c2c0d9546f1dd643aafcfa8
mohsin0176 <mohsin2010ju@gmail.com>
2021-05-02 08:34:26 -0700

    mailer php frontend  coded

--- mail_handler.php
+++ mail_handler.php
@@ -6,7 +6,7 @@
 		$msg=$_POST['msg'];  
 		$subject='From HR';
 		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-		$headers="From: ".$email;
+		$headers="From: ".$to;
 
 		if(mail($to, $subject, $message, $headers)){
 			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
